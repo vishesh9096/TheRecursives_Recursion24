d696f48386f3041e5dcf0035bfe635cf8f93ab95
Vishesh Gatha <visheshgatha@Visheshs-MacBook-Air.local>
2024-03-13 14:36:31 +0530

    assets and components

---
+++ src/Lib/Config.js
@@ -0,0 +1,7 @@
+export default {
+    app_name: "SIH_2023_APP",
+    // url: "https://legalbridge-backend.vercel.app/api/"
+    url: "https://onehealthassist.com/"
+
+
+}
---
+++ src/contants/colors.js
@@ -0,0 +1,30 @@
+const colors = {
+    // primary: '#07665a',
+    
+    primary: '#3C854D',
+    btnBack: '#08A4A4',
+    white: '#FFFFFF',
+    black: '#000000',
+    grey: '#484848',
+    primarydark: "#D46002",
+    blackdark: "#262626",
+    inputColorP: "#8F8F8F",
+    grey2: "#D9D8D6",
+    newGrey: '#CFCFCF',
+    menbg: "#D08962",
+    inputBorder: '#CA8A66',
+    red: '#EF1A1A',
+    green: '#00891E',
+    greyNoti: '#F4F4F4',
+    modalBg: '#00000095',
+    brownColo: '#A44E00',
+    // drakGreen: '#0F542B',
+    // orange: '#FF9900',
+    // bgTab: '#007638',
+    // blue: '#0C05A7',
+    // inputColo: '#F6F9FF91',
+    // headerTitleColo: '#616163',
+    // cardColo: '#F2F2F2',
+};
+
+export default colors;
